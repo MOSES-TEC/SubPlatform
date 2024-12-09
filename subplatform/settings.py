30d2f2c52ce712c287b13b668cf6abf5af6770aa
MOSES Y <154627598+MOSES-TEC@users.noreply.github.com>
2024-12-09 19:10:41 +0530

    Update settings.py

--- subplatform/settings.py
+++ subplatform/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nimorathought-subplatform.onrender.com', '*']
 
-# CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['https://nimorathought-subplatform.onrender.com']
 
 
 # Application definition
